558a64f74f687c00ab45e9c990dd7414db5391cb
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-05 14:27:35 -0500

    Add Vuetify and @mdi/font dependencies

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,5 @@
 import './assets/main.css'
+import '@mdi/font/css/materialdesignicons.css' // Ensure you are using css-loader
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
@@ -8,12 +9,27 @@ import router from './router'
 import { VueFire } from 'vuefire'
 import { firebaseApp } from './conf/firebase'
 
+// Vuetify
+import 'vuetify/styles'
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+
 const app = createApp(App)
+
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi'
+  }
+})
 
 app.use(createPinia())
 app.use(router)
 app.use(VueFire, {
   firebaseApp: firebaseApp
 })
+app.use(vuetify)
 
 app.mount('#app')
