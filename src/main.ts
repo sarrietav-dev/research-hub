dd63df3149a4f6da78a857d14468836c353a642a
Sebastian <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-05 14:13:42 -0500

    Add VueFire and Firebase configuration

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,15 @@ import { createPinia } from 'pinia'
 
 import App from './App.vue'
 import router from './router'
+import { VueFire } from 'vuefire'
+import { firebaseApp } from './conf/firebase'
 
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(VueFire, {
+  firebaseApp: firebaseApp
+})
 
 app.mount('#app')
