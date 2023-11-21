aa453031da149ef6f4421e0eba1c4fdcce278f1b
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-21 17:50:07 -0500

    Add dependencies and JWT configuration

--- apps/server/src/auth/auth.module.ts
+++ apps/server/src/auth/auth.module.ts
@@ -3,9 +3,21 @@ import { AuthService } from './services/auth/auth.service';
 import { UserService } from './services/user/user.service';
 import { PrismaService } from '@/prisma/prisma.service';
 import { CryptoService } from './services/crypto/crypto.service';
+import { JwtModule } from '@nestjs/jwt';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   providers: [AuthService, UserService, CryptoService],
-  imports: [PrismaService],
+  imports: [
+    PrismaService,
+    JwtModule.registerAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => ({
+        secret: config.get<string>('JWT_SECRET'),
+        signOptions: { expiresIn: '1h' },
+      }),
+    }),
+  ],
 })
 export class AuthModule {}
