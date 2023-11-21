29a1871ca7f3ef868f0a81abfbbb0c6fd0a4687c
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-21 18:01:10 -0500

    Add signIn method to AuthService

--- apps/server/src/auth/services/auth/auth.service.ts
+++ apps/server/src/auth/services/auth/auth.service.ts
@@ -1,4 +1,36 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { UserService } from '../user/user.service';
+import { JwtService } from '@nestjs/jwt';
+import { CryptoService } from '../crypto/crypto.service';
 
 @Injectable()
-export class AuthService {}
+export class AuthService {
+  constructor(
+    private userService: UserService,
+    private jwtService: JwtService,
+    private cryptoService: CryptoService,
+  ) {}
+
+  async signIn(email: string, password: string) {
+    const user = await this.userService.getUserByEmail(email);
+
+    if (!user) {
+      throw new UnauthorizedException();
+    }
+
+    const passwordMatch = await this.cryptoService.compare(
+      password,
+      user.password,
+    );
+
+    if (!passwordMatch) {
+      throw new UnauthorizedException();
+    }
+
+    const payload = { id: user.id };
+
+    return {
+      accessToken: this.jwtService.sign(payload),
+    };
+  }
+}
