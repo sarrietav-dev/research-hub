f57bff705588c15143bd34d9b62727ce6684bab3
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-22 20:13:31 +0000

    Create program module

--- apps/server/src/app.module.ts
+++ apps/server/src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { PrismaService } from './prisma/prisma.service';
 import { SharedModule } from './shared/shared.module';
+import { ProgramsModule } from './programs/programs.module';
 
 @Module({
-  imports: [AuthModule, SharedModule],
+  imports: [AuthModule, SharedModule, ProgramsModule],
   providers: [PrismaService],
   exports: [PrismaService],
 })
