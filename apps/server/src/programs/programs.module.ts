029708720f1ab6e25d6537eb39ed2fc3f23fec8f
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-11-22 20:39:50 -0500

    Add PrismaService to ProgramsModule

--- apps/server/src/programs/programs.module.ts
+++ apps/server/src/programs/programs.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProgramService } from './services/program/program.service';
 import { ProgramRepositoryService } from './services/program-repository/program-repository.service';
+import { PrismaService } from '@/prisma/prisma.service';
 
 @Module({
-  providers: [ProgramService, ProgramRepositoryService]
+  providers: [ProgramService, ProgramRepositoryService, PrismaService],
 })
 export class ProgramsModule {}
