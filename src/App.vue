2aed14d8da291f64387cff151cd2dcfe3a4f5ddb
Fleesin <jean.quintana.123@gmail.com>
2023-11-23 23:52:59 -0500

    add StudentList with searching

--- src/App.vue
+++ src/App.vue
@@ -9,11 +9,22 @@
       <v-btn variant="tonal" prepend-icon="mdi-login">Login</v-btn>
     </v-app-bar>
       <v-main >
-      
       <v-container>
-        <router-view></router-view>
+        <StudentList></StudentList>
       </v-container>
       
       </v-main>
   </v-app>
 </template>
+
+<script>
+  import StudentList from './components/StudentList.vue'
+
+  export default {
+    name: 'App',
+    components:{
+      StudentList
+    }
+  }
+
+</script>
