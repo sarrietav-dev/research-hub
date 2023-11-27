5096e0aed3b29fcf2b87e6e71ff6956b7466a5db
sebasjar <148925579+sebasjar@users.noreply.github.com>
2023-11-26 21:58:52 -0500

    Updated view and database connections (#33)
    * Updated ReadME 😎
    
    Help
    
    * Update
    
    * Update README.md
    
    * Updated GroupView
    
    Updated groupview with the new database system
    
    * Uptaded database connections

--- apps/client/src/views/GroupView.vue
+++ apps/client/src/views/GroupView.vue
@@ -1,39 +1,40 @@
-<script>
+<script setup lang="ts">
 import axios from 'axios';
-export default {
-  data() {
-    return {
-      dataListPrograms: [],
-      dataListSeeds: []
-    };
-  },
-  mounted() {
-    axios.get('http://localhost:3000/api/programs')
-      .then(response => {
-        this.dataListPrograms = response.data
-          console.log(this.dataListPrograms)
-        })
-      .catch(error => {
-        console.log(error);
-      });
+import { useRouter } from 'vue-router';
+import { onMounted, ref } from 'vue'
 
-    axios.get('http://localhost:3000/api/seed-groups')
-    .then(response => {
-      this.dataListSeeds = response.data
-      console.log(this.dataListSeeds)
-    })
-    .catch(error => {
-      console.log(error)
-    })
+const router = useRouter()
+function onClick(name: string, id: any){
+  router.push({path: `/seed_groups/${name}`, query: { id }})
+}
+
+const dataListPrograms = ref()
+const dataListSeeds = ref()
+
+onMounted(async () => {
+  try {
+    const responseProg = await axios.get('http://localhost:3000/api/programs')
+    dataListPrograms.value = responseProg.data
+    console.log(dataListPrograms.value)
+
+    const responseSeed = await axios.get('http://localhost:3000/api/seed-groups')
+    dataListSeeds.value = responseSeed.data
+    console.log(dataListSeeds.value)
+
+  } catch(error){
+    console.log(error)
   }
-};
+
+})
 </script>
 <template>
   <v-expansion-panels>
     <v-expansion-panel v-for="item in dataListPrograms" :key="item">
       <v-expansion-panel-title>{{ item.name }}</v-expansion-panel-title>
       <v-expansion-panel-text>
-        <v-list v-if="dataListSeeds.programId == dataListPrograms.id " :items="dataListSeeds" item-title="name" item-value="name">
+        <v-list v-if="dataListSeeds.programId == dataListPrograms.id">
+          <v-list-item @click = "onClick(seed.name, seed.id)" v-for="seed in dataListSeeds" :key="seed" :title="seed.name" :value="seed.name">
+          </v-list-item>
         </v-list>
       </v-expansion-panel-text>
     </v-expansion-panel>
--- apps/client/src/views/SeedView.vue
+++ apps/client/src/views/SeedView.vue
@@ -1,65 +1,78 @@
+<script setup lang = "ts">
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+
+const seed_information = ref()
+const seed_members = ref()
+const isCharged = ref()
+onMounted( async () => {
+    try {
+        const url = "http://localhost:3000/api/seed-groups/" + new URL(location.href).searchParams.get('id')
+
+        isCharged.value = true
+        const responseGroups = await axios.get(url)
+        const responseMembers = await axios.get(url + "/members")
+        isCharged.value = false
+
+        seed_information.value = responseGroups.data
+        seed_members.value = responseMembers.data
+        console.log(seed_information.value)
+        console.log(seed_members.value)
+
+    }catch(error){
+        console.log(error)
+    }
+})
+
+
+</script>
 <template>
     <div id="app">
         <v-container>
-            <v-layout>
-                <div v-if="posts">
-                    <v-data-table v-model:items-per-page="itemsPerPage">
-                        :headers="headers"
-                        :items="data"
-                        item-value="userID"
-                        class="elevation-1"
-                    </v-data-table>
-                </div>
+            <v-layout v-if="isCharged == false">
+                <v-card class="overflow-auto">
+                    <v-card-title primary-title class="headline font-weight-bold text-xs-center">
+                        {{ seed_information.name }} ({{ seed_information.id }})
+                    </v-card-title>
+                    <v-card-subtitle class="font-weight-bold subtile"
+                        v-for = "item in seed_information.researchLines" :key = "item">
+                        {{ item }}
+                    </v-card-subtitle>
+                    <v-card-text>
+                        {{ seed_information.description }}
+                    </v-card-text>
+                    <v-card-title>
+                        Miembros
+                    </v-card-title>
+                    <v-card-text>
+                        <ul v-for = "item in seed_members" :key = "item">
+                            ◽ {{ item.member.name }} | <b>{{ item.role }} </b>
+                        </ul>
+                    </v-card-text>
+                </v-card>
+                <v-expansion-panels variant = "inset">
+                    <h2>Proyectos</h2>
+                    <v-expansion-panel v-for = "item in seed_information.projects" :key = "item">
+                        <v-expansion-panel-title>{{ item.name }}</v-expansion-panel-title>
+                        <v-expansion-panel-text> 
+                            Dinero Aprobado: <b>{{ item.approvedAmount }}</b>
+                            <h4>Productos</h4>
+                            <v-list>
+                                <v-list-item-content v-for = "item2 in item.products " :key = "item2"> 
+                                    <v-list-item-title class="text-wrap"> {{ item2.name }} </v-list-item-title>
+                                    <v-list-item-subtitle class = "mb-4"> {{  item2.description }}</v-list-item-subtitle>
+                                </v-list-item-content>
+                            </v-list>
+                        </v-expansion-panel-text>
+                    </v-expansion-panel>
+                </v-expansion-panels>
+            </v-layout>
+            <v-layout v-else>
+                <v-progress-circular class="mx-auto"
+                    indeterminate
+                    color="primary"
+                ></v-progress-circular>
             </v-layout>
         </v-container>
     </div>
 </template>
-
-<style>
-    .my-custom-list-item {
-        margin-bottom: -20px;
-    }
-    .subtitle{
-        height: auto;
-        color: black;
-    }
-</style>
-
-
-<script>
-
-    import getAllPosts from '../services/getAllPosts.js';
-    
-    export default{
-        components:{
-            VDataTable
-        },
-        setup(){
-            
-            const {posts, error, load} = getAllPosts()
-            load()
-            return {posts, error, load, itemsPerPage: 10, headers:[{
-                title: 'Código',
-                align: 'start',
-                sortable: false,
-                key: 'userID'
-            },
-            {
-                title: 'Title',
-                align: 'start',
-                key: 'title'
-            },
-            {
-                title: 'Body',
-                align: 'start',
-                key: 'body'
-            },
-        
-        ],
-        data: posts
-            }
-        }
-    }
-
-    
-</script>
