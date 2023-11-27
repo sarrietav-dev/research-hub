28c2d773109c33cd41f5422dd6964eb5c83649d8
Margaret G. <margaretprogramming@gmail.com>
2023-11-26 21:43:02 -0500

    Login con Css

---
+++ apps/client/src/views/FormGroupView.vue
@@ -0,0 +1,61 @@
+<template>
+    <main>
+      <div class="login">
+        <h1>Login</h1>
+        <div class="groups">
+            <div class="new_group_old_group">
+                <label for="new_group_old_group">Desea usar un grupo de investigación que ya existe:</label>
+                <label><input type="radio" id="old_group" name="new_group_old_group">Si</label>
+                <label><input type="radio" id="new_group" name="new_group_old_group">No</label>
+            </div>
+            <div class="new_group">
+                <label for="new_group">Grupo de Investigación:</label>
+                <input type="text" name="new_group">
+            </div>
+            <div class="new_seed_g_old_seed_g">
+                <label for="new_seed_g_old_seed_g">Desea usar un semillero de investigación que ya existe:</label>
+                <label><input type="radio" id="old_seed_g" name="new_seed_g_old_seed_g">Si</label>
+                <label><input type="radio" id="new_seed_g" name="new_seed_g_old_seed_g">No</label>
+            </div>
+            <div class="new_seed_group">
+                <label for="new_seed_group">Semillero de Investigación:</label>
+                <input type="text" name="new_seed_group" >
+            </div>
+        </div>
+        <div class="new_student_old_student">
+                <label for="new_student_old_student">Desea usar un semillero de investigación que ya existe:</label>
+                <label><input type="radio" id="old_seed_g" name="new_student_old_student">Si</label>
+                <label><input type="radio" id="new_seed_g" name="new_student_old_student">No</label>
+            </div>
+        <div class="new_student_info">
+            <div class="student_name">
+                <label for="student_name">Nombre del estudiante:</label>
+                <input type="text" name="student_name" >
+            </div>
+            <div class="student_program">
+                <label for="student_program">Programa del estudiante:</label>
+                <input type="text" name="student_program" >
+            </div>
+            <div class="student_email">
+                <label for="student_email">Email del estudiante:</label>
+                <input type="email" name="student_email" >
+            </div>
+        </div>
+        <button>Send</button>
+      </div>
+    </main>
+  </template>
+
+<style>
+.new_group_old_group .new_group .new_seed_g_old_seed_g .new_seed_g {
+    display: none;
+}
+
+#new_group:checked + .new_group {
+    display: block;
+}
+
+#new_seed_g:checked + .new_seed_g {
+    display: block;
+}
+</style>
--- apps/client/src/views/LoginView.vue
+++ apps/client/src/views/LoginView.vue
@@ -1,7 +1,7 @@
 <template>
   <main>
     <div class="login">
-      <h1>Login</h1>
+      <h1 class="text-center">Login</h1><br>
       <div class="user">
         <label for="user">Email:</label>
         <input type="email" name="user" v-model="user">
@@ -10,28 +10,72 @@
         <label>Password:</label>
         <input type="password" name="passw" v-model="passw">
       </div>
-      <button>Send</button>
+      <button @click="login">Send</button>
     </div>
   </main>
 </template>
 
-<script setup lang="ts">
+<style>
+ .login{
+  box-shadow: 2px 2px 2px 2px rgba(0, 0, 0, 0.2);
+  border-radius: 20px;
+  padding: 20px;
+  position: absolute;
+  align-content: center;
+  height: 390px;
+  width: 320px;
+  transform: translate(140%, 15%)
+ }
+
+ input{
+  margin: 5px 2px 15px 0px;
+  padding-left: 12px;
+  width: 100%;
+  min-height: 2.5em;
+  background-color: #ffffff;
+  border: 1px solid #bdbdbd;
+  border-radius: 5px;
+  color: #000000;
+ }
+
+ button {
+    display: block;
+    width: 60%;
+    margin: 1em auto;
+    height: 2em;
+    font-size: 1.1rem;
+    background: #3271a5;
+    border-radius: 20px;
+    border: none;
+    color: white;
+    min-width: 280px;
+  }
+
+ label {
+  padding-bottom: 2px;
+ }
+</style>
+
+<script lang="ts">
+import { group } from 'console';
   export default{
     name:'Login',
     data () {
       return {
         user: '',
-        password: ''
+        passw: ''
       }
     },
     methods: {
       login(){
-        if(this.user === && this.password === ){
-
-        }
-        else {
-          alert('Error, Usuario o contraseña incor')
-        }
+        //for (i in range (group.length) ){  
+          if(this.user === "admin@unicartagena.edu.co" && this.passw === "abc"){
+            alert('Entraste yei')
+          }
+          else {
+            alert('Error, Usuario o contraseña incorrecto')
+          }
+        //}
       }
     }
   }
