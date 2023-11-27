a505ba6fdaec6a0e34ce7816b62cb83e24418db5
Fleesin <jean.quintana.123@gmail.com>
2023-11-27 00:40:11 -0500

    Fixed visuals in views (Seed, StudentList, Group)

--- apps/client/src/components/StudentList.vue
+++ apps/client/src/components/StudentList.vue
@@ -1,38 +1,42 @@
 <template>
-    <v-card flat>
-      <v-card-title class="d-flex align-center pe-2">
-        <v-icon icon="mdi-account-group"></v-icon> &nbsp;
-        Integrantes
-  
-        <v-spacer></v-spacer>
-  
-        <v-text-field
-          v-model="search"
-          prepend-inner-icon="mdi-magnify"
-          density="compact"
-          label="Search"
-          single-line
-          flat
-          hide-details
-          variant="solo-filled"
-        ></v-text-field>
-      </v-card-title>
-  
-      <v-divider></v-divider>
-      <v-data-table v-model:search="search" :items="items">
+  <v-card flat>
+    <v-card-title class="d-flex align-center pe-2">
+      <v-icon icon="mdi-account-group"></v-icon> &nbsp;
+      Integrantes
 
-      </v-data-table>
-    </v-card>
-  </template>
+      <v-spacer></v-spacer>
 
-  <script>
-    export default {
-      data () {
-        return {
-          search: '',
-          items: [
-          ],
-        }
-      },
-    }
-  </script>
+      <v-text-field
+        v-model="search"
+        prepend-inner-icon="mdi-magnify"
+        density="compact"
+        label="Search"
+        single-line
+        flat
+        hide-details
+        variant="solo-filled"
+      ></v-text-field>
+    </v-card-title>
+
+    <v-divider></v-divider>
+    <v-data-table v-model:search="search" :items="items">
+
+    </v-data-table>
+  </v-card>
+</template>
+
+<script>
+export default {
+  props: {
+    items: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  data() {
+    return {
+      search: '',
+    };
+  },
+};
+</script>
