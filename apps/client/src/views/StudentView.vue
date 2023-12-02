5e551120067876a2eef5652664fe396a2ee7abd1
Sebastian Arrieta <25210925+sarrietav-dev@users.noreply.github.com>
2023-12-02 15:35:16 -0500

    Fix url at studentView

--- apps/client/src/views/StudentView.vue
+++ apps/client/src/views/StudentView.vue
@@ -1,41 +1,42 @@
 
-  <script setup lang = "ts">
-  import axios from 'axios';
-  import { onMounted, ref } from 'vue';
-  
-  const memberInfo= ref()
-  const memberHistory= ref()
-  const isCharged = ref()
-  
-  onMounted( async () => {
-      try {
-        const url = "http://localhost:3000/api/members/" + new URL(location.href).searchParams.get('id')
-        isCharged.value = true
-        const responseMember = await axios.get(url)
-        const responseHistory = await axios.get(url + "/seed-groups?seedGroupId=" + new URL(location.href).searchParams.get('id'))
-        isCharged.value = false
-        memberHistory.value = responseHistory
-        memberInfo.value = responseMember.data
-        console.log(memberInfo.value)
-        console.log(memberHistory.value)
-      }catch(error){
-          console.log(error)
-      }
+<script setup lang = "ts">
+import axios from 'axios';
+import { onMounted, ref } from 'vue';
+import baseUrl from '@/lib/baseUrl';
+
+const memberInfo = ref()
+const memberHistory = ref()
+const isCharged = ref()
+
+onMounted(async () => {
+  try {
+    const url = `${baseUrl}/api/members/` + new URL(location.href).searchParams.get('id')
+    isCharged.value = true
+    const responseMember = await axios.get(url)
+    const responseHistory = await axios.get(url + "/seed-groups?seedGroupId=" + new URL(location.href).searchParams.get('id'))
+    isCharged.value = false
+    memberHistory.value = responseHistory
+    memberInfo.value = responseMember.data
+    console.log(memberInfo.value)
+    console.log(memberHistory.value)
+  } catch (error) {
+    console.log(error)
   }
-  )
-  </script>
+}
+)
+</script>
 
 <template>
-    <div id="app">
-        <v-container class="flex-1-1-100 ma-2 pa-2">
-            <v-layout v-if="isCharged == false">
-                <v-card class="overflow-auto">
-                    <v-card-title primary-title class="headline font-weight-bold text-xs-center">
-                        {{ memberInfo.name }} ({{ memberInfo.id }})
-                    </v-card-title>
-                </v-card>  
-            </v-layout>
-        </v-container>
-    </div>
+  <div id="app">
+    <v-container class="flex-1-1-100 ma-2 pa-2">
+      <v-layout v-if="isCharged == false">
+        <v-card class="overflow-auto">
+          <v-card-title primary-title class="headline font-weight-bold text-xs-center">
+            {{ memberInfo.name }} ({{ memberInfo.id }})
+          </v-card-title>
+        </v-card>
+      </v-layout>
+    </v-container>
+  </div>
 </template>
   
